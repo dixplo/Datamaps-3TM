8aa61f03f1d48870a460b75d2bbb0fe9170b432e
ThomasFont3721 <thomas101000@gmail.com>
2019-10-18 09:14:11 +0200

    first commit

---
+++ src/main/java/fr/tm/datasmap/DatasMapApplication.java
@@ -0,0 +1,13 @@
+package fr.tm.datasmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tm/datasmap/DatasMapApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tm.datasmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
