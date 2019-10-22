caa4716fd5cc2901081b8e517dfd3896fef32945
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2019-10-22 15:25:32 +0200

    ajout des class

---
+++ src/main/java/fr/tm/datasmap/entity/Application.java
@@ -0,0 +1,46 @@
+package fr.tm.datasmap.entity;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Application {
+	private static List<Place> places;
+	private static List<Type> types;
+	
+	public static List<Type> loadDefaultTypes(){
+		return null;
+	}
+	public static List<Place> loadDefaultPlaces(){
+		places=new ArrayList<Place>();
+		Place pFrance=new Place("France",46.467196,2.151961);
+		Place normandie=new Place("Normandie",48.950632,0.167025,pFrance);
+		Place caen=new Place("Caen",49.185402,-0.371057,normandie);
+		Place wurz=new Place("Place Würzburg",49.201974,-0.379424,caen);
+		Place lycee=new Place("1",49.201771,-0.380820,wurz);
+		Collections.addAll(places, pFrance,normandie,caen,wurz,lycee);
+		return places;
+	}
+	
+	public List<Place> getPlacesByAddress(String address){
+		return null;
+	}
+	
+	public List<Place> getPlacesByGps(double latitude,double longitude){
+		return null;
+	}
+	
+	public List<Place> getPlacesByEvtGroups(String groupName){
+		return null;
+	}
+	
+	public List<Place> getPlacesByEvtGroups(String groupName,LocalDateTime fromDate){
+		return null;
+	}
+	
+	public List<Place> getPlacesByEvtGroups(String groupName,LocalDateTime fromDate,LocalDateTime toDate){
+		return null;
+	}
+}
---
+++ src/main/java/fr/tm/datasmap/entity/Event.java
@@ -0,0 +1,25 @@
+package fr.tm.datasmap.entity;
+
+
+import java.time.LocalDateTime;
+
+public class Event {
+	private String name;
+	private String description;
+	private LocalDateTime date;
+	private String groups;
+	
+	public Event(String name,String description,String groups) {
+		
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param groupName
+	 * @return
+	 */
+	public boolean hasGroup(String groupName) {
+		return false;
+	}
+}
