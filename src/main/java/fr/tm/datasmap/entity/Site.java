cf62f30f612760fe4b807d73a9dd9f8f71688db7
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2019-10-22 15:25:32 +0200

    ajout des class

---
+++ src/main/java/fr/tm/datasmap/entity/Site.java
@@ -0,0 +1,20 @@
+package fr.tm.datasmap.entity;
+
+
+public class Site {
+	private String name;
+	private String description;
+	
+	public Site(String name,String description,String address) {
+		
+	}
+	
+	public Site(String name,String description,double latitude,double longitude) {
+		
+	}
+	
+	public Site(String name,String description,Place place) {
+		
+	}
+
+}
