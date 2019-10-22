bd136f4a8cd6f288259ce0c59676bee26fbdf2bb
ThomasFont3721 <thomas101000@gmail.com>
2019-10-22 14:01:07 +0200

    bases

---
+++ src/main/java/fr/tm/datasmap/AppConfiguration.java
@@ -0,0 +1,9 @@
+package fr.tm.datasmap;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.github.jeemv.springboot.vuejs")
+public class AppConfiguration {
+}
