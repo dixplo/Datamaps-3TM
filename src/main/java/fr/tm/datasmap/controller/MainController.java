ef6e43abf975a7d70d5a2e51aa69395bd45705b6
ThomasFont3721 <thomas101000@gmail.com>
2019-10-22 15:29:31 +0200

    ajout d'un class user

---
+++ src/main/java/fr/tm/datasmap/controller/MainController.java
@@ -0,0 +1,10 @@
+package fr.tm.datasmap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/map/")
+public class MainController {
+
+}
