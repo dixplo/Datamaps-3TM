b21785b6dd16cd98f9eec8c6f9313b21c01b6583
theomarie <theomarie.com>
2019-11-03 18:47:55 +0100

    rajout

--- src/main/java/fr/tm/datasmap/controller/MainController.java
+++ src/main/java/fr/tm/datasmap/controller/MainController.java
@@ -1,10 +1,23 @@
 package fr.tm.datasmap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.jeemv.springboot.vuejs.VueJS;
 
 @Controller
 @RequestMapping("/map/")
 public class MainController {
+	
+	@Autowired
+	private VueJS vue;
 
+	@GetMapping("")
+	public String map(ModelMap map) {
+		map.put("vue", vue);
+		return "map/indexmap";
+	}
 }
