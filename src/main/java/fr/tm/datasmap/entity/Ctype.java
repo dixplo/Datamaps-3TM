eccb7fd61796a5499484cf33de7c6c4cdea1f240
theomarie <theomarie.com>
2019-11-05 10:48:38 +0100

    Ctype

---
+++ src/main/java/fr/tm/datasmap/entity/Ctype.java
@@ -0,0 +1,17 @@
+package fr.tm.datasmap.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Ctype {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@ManyToOne
+	private Cuser user;
+}
