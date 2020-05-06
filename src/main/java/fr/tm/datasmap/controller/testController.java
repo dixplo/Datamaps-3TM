611175bef1b01a293e1518133cdcc30b03b0efef
theomarie <theomarie.com>
2020-05-06 22:32:16 +0200

    teste controler route 2

--- src/main/java/fr/tm/datasmap/controller/testController.java
+++ src/main/java/fr/tm/datasmap/controller/testController.java
@@ -3,6 +3,7 @@ package fr.tm.datasmap.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,18 +12,17 @@ import io.github.jeemv.springboot.vuejs.VueJS;
 
 @Controller
 public class testController {
-	
 	@Autowired
 	private VueJS vue;
 
-	@GetMapping(name = "vue")
-	public @ResponseBody String helloW(){
-		return "Hello Wolrd !";
+	@ModelAttribute(name = "vue")
+	private VueJS getVue() {
+		return this.vue;
 	}
-	
+
 	@RequestMapping("/test")
-	public String maps(ModelMap map){
-		map.put("vue", vue);
-		return "index";
+	public String test() {
+		vue.addData("message", "Hello world");
+		return "test";
 	}
 }
